9a6b743f11857dcdb5bfdad12b12f97a05146458
akbar <moakbar202@gmail.com>
2024-10-15 18:48:59 +0100

    url matching works

--- background.js
+++ background.js
@@ -1,23 +1,32 @@
-const matchPatterns = ["*://*.mozilla.org/*", "*://*.youtube.com/*"];
+
+const URLS = ["mozilla.org", "youtube.com"];
 
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-
-
     console.log(tab.url);
+    console.log(matchPattern(tab.url));
 
-
-
-    if (matchPatterns.some(pattern => urlMatchesPattern(tab.url, pattern))) {
-        chrome.tabs.executeScript(tabId, {
-            file: 'oneMoment.js'
-        });
-    }
+    // if (matchPatterns.some(pattern => urlMatchesPattern(tab.url, pattern))) {
+    //     chrome.tabs.executeScript(tabId, {
+    //         file: 'oneMoment.js'
+    //     });
+    // }
 });
 
 
 
-function matchPattern(url)
+function matchPattern(tabUrl)
 {
-    
+    let returnValue = false;
+    URLS.forEach(url =>{
+        result = url.split(".");
+        SLD = result[0];
+        TLD = result[1];
+        let pattern = `^(https?:\\/\\/)?([^\\/]+\\.)?${SLD}\\.${TLD}\\/.*$`;
+        let regex = new RegExp(pattern);
+        if(regex.test(tabUrl)){
+            returnValue = true; 
+        }
+    });
+    return returnValue;
 }
